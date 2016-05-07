0c1ebb9308683b75b07d9f907a7e8a371c3917aa
Dan Appel <Dan.appel00@gmail.com>
2016-05-07 15:24:35 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "SessionMiddleware",
     dependencies: [
         .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 6),
-        .Package(url: "https://github.com/Danappelxx/UUID.git", majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/Zewo/UUID.git", majorVersion: 0, minor: 2)
     ]
 )
